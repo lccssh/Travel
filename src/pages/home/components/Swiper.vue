33b091c14d08846090853d47dbe11021ee0a33a3
Your Name <you@example.com>
2018-08-12 21:29:17 +0800

    change

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true // 轮播图循环播放
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1806/3c/c72a1ccd4d7b2202.jpg_750x200_b88bbab4.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/5e/514ce60160e62602.jpg_750x200_9b0074e2.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper >>> .swiper-pagination-bullet-active {
+    background: #fff;
+  }
+  .wrapper {
+    width: 100%;
+    height: 0;
+    overflow: hidden;
+    padding-bottom: 26.67%;
+    background: #eee;
+  }
+  .swiper-img {
+    width: 100%
+  }
+</style>
