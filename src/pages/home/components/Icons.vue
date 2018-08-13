3ec1eea4f7476e75e64e3850b6e6eb26dca02562
Your Name <you@example.com>
2018-08-13 12:47:21 +0800

    add icons

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.imgUrl" />
+          </div>
+          <p class="icon-desc">{{item.desc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      iconList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/96/c70f1e85ae4a4f02.png',
+        desc: '自然风光'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/f5/a963333e1fa802.png',
+        desc: '自然风光'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/50/26ffa31b56646402.png',
+        desc: '海洋馆'
+      }, {
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+        desc: '芜湖必游'
+      }, {
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/95/8d02011d149bdb02.png',
+        desc: '汽车票'
+      }, {
+        id: '0007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/8246f27355943202.png',
+        desc: '芜湖方特'
+      }, {
+        id: '0008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/90/59a2f523ee2f9202.png',
+        desc: '西递宏村'
+      }, {
+        id: '0009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/97/02f5043b51b2102.png',
+        desc: '黄山'
+      }]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .icons >>> .swiper-container {
+    height: 0;
+    padding-bottom: 50%;
+  }
+  .icons .icon {
+    position: relative;
+    overflow: hidden;
+    float: left;
+    width: 25%;
+    height: 0;
+    padding-bottom: 25%;
+  }
+  .icons .icon .icon-img {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: .44rem;
+    box-sizing: border-box;
+    padding: .1rem;
+  }
+  .icons .icon .icon-img .icon-img-content {
+    height: 100%;
+    display: block;
+    margin: 0 auto;
+  }
+  .icons .icon .icon-desc {
+    position: absolute;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    height: .44rem;
+    line-height: .44rem;
+    text-align: center;
+    color: #333;
+    padding-bottom: .1rem;
+  }
+</style>
